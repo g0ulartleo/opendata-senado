5a9d021b587d2abeb62addc0ca527b3b5a177398
Leonardo Goulart <goulart@navegg.com>
2017-05-17 13:13:08 -0300

    added project structure

---
+++ opensenate/__init__.py
@@ -0,0 +1,1 @@
+from .senator import Senator
---
+++ opensenate/senator.py
@@ -0,0 +1,10 @@
+import sys
+
+SENATE_BASE_URL = "http://legis.senado.gov.br/dadosabertos/"
+
+class Senator(object):
+
+    senators_relative_url = "senador/lista/atual"
+
+    def __init__(self):
+        pass
